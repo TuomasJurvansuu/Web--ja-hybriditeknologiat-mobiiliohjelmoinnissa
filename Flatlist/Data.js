c9e96d0b463143b9a211289ce19194a26d97b587
TuomasJurvansuu <88161273+TuomasJurvansuu@users.noreply.github.com>
2023-10-02 13:39:35 -0700

    Flatlist

---
+++ Flatlist/Data.js
@@ -0,0 +1,10 @@
+export const DATA = [
+  { id: '1', firstname: 'Eero', lastname: 'Hannula' },
+  { id: '2', firstname: 'Hannu', lastname: 'Virtanen' },
+  { id: '3', firstname: 'Matti', lastname: 'Nykänen' },
+  { id: '4', firstname: 'Patrik', lastname: 'Laine' },
+  { id: '5', firstname: 'Topias', lastname: 'Uusitalo' },
+  { id: '6', firstname: 'petri', lastname: 'Nygård' },
+  { id: '7', firstname: 'Donald', lastname: 'Trump' },
+  { id: '8', firstname: 'Pekka', lastname: 'Haavisto' },
+];
